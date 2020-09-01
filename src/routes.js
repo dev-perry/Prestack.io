46cbc473be9d324bcd1a3bd9c6c3172e5986a31c
perry <pasibeybonsu@drew.edu>
2020-09-01 14:36:29 -0400

    Routes rendered by core component in single file

---
+++ src/routes.js
@@ -0,0 +1,57 @@
+import Drive from './pages/Drive';
+// {
+//   path: "/path-name",
+//   name: "Drive",
+//   icon: "icon name",
+//   component: Drive,
+//   layout: "layout-tag"
+// },
+
+const routes = [
+  {
+    path: "/drive",
+    name: "Drive",
+    icon: "fas fa-hdd",
+    component: Drive,
+    layout: "admin"
+  },
+  {
+    path: "/presentations",
+    name: "Presentations",
+    icon: "fas fa-bolt",
+    component: null,
+    layout: "admin"
+  },
+  {
+    path: "/participation",
+    name: "Participation",
+    icon: "fas fa-user-check",
+    component: null,
+    layout: "admin"
+  },
+  {
+    collapse: true,
+    name: "Classes",
+    icon: "fas fa-school",
+    state: "classesCollapse",
+    views: [
+      {
+        path: "/classes/french-201-kjdlsauhelfaeju",
+        name: "French 201",
+        miniName: "F",
+        component: null,
+        layout: "admin"
+      },
+      {
+        path: "/classes/phys-150-jkfurehliufheue",
+        name: "Physics 150",
+        miniName: "P",
+        component: null,
+        layout: "/auth"
+      }
+    ]
+  }
+
+];
+
+export default routes;
