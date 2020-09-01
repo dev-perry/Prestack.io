8692f126613da45c896f7d2aa84eb7ef8d0a14fe
perry <pasibeybonsu@drew.edu>
2020-09-01 14:36:40 -0400

    Redux store configured

---
+++ src/store/configureStore.js
@@ -0,0 +1,18 @@
+import {applyMiddleware, createStore, compose} from "redux";
+import thunkMiddleware from "redux-thunk";
+
+import {verifyAuth} from "../actions";
+import rootReducer from "../reducers";
+
+export default function configureStore(persistedState){
+  const store = createStore(
+    rootReducer,
+    persistedState,
+    compose(
+      applyMiddleware(thunkMiddleware),
+      window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+    )
+  );
+  store.dispatch(verifyAuth());
+  return store;
+}
