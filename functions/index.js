6d2fb27c69ee3f3f58f83e9a0b7066908004fb8b
perry <pasibeybonsu@drew.edu>
2020-09-05 00:29:15 -0400

    New functions for drive uploads and deletes

--- functions/index.js
+++ functions/index.js
@@ -2,25 +2,39 @@ const functions = require('firebase-functions');
 const admin = require('firebase-admin');
 admin.initializeApp();
 
-//Functions for user signups
-exports.createUserDoc = functions.https.onCall((data) => {
-  return admin.firestore().collection('users').doc(data.uid).set({
-    email: data.email,
-    displayName: data.displayName,
-    billing: false
-  })
+//Functions for drive uploads/deletes
+exports.addedToDrive = functions.https.onCall((data) => {
+  return admin.firestore().collection('users').doc(data.uid).collection('drive')
+  .doc(data.info.name).set({
+    uploaded: new Date(data.info.timeCreated),
+    size: data.info.size,
+    type: data.type
+  });
 });
-exports.createUserDocProvider = functions.auth.user().onCreate((user) => {
-  if(user.providerData[0].providerId !== "password"){
-    return admin.firestore().collection('users').doc(user.uid).set({
-      email: user.providerData[0].email,
-      displayName: user.providerData[0].displayName,
-      billing: false
-    })
-  }else{
-    return null;
-  }
-});
+exports.deletedFromDrive = functions.https.onCall((data) => {
+  return admin.firestore().collection('users').doc(data.uid).collection('drive')
+  .doc(data.info.name).delete()
+})
+
+// //Functions for user signups
+// exports.createUserDoc = functions.https.onCall((data) => {
+//   return admin.firestore().collection('users').doc(data.uid).set({
+//     email: data.email,
+//     displayName: data.displayName,
+//     billing: false
+//   })
+// });
+// exports.createUserDocProvider = functions.auth.user().onCreate((user) => {
+//   if(user.providerData[0].providerId !== "password"){
+//     return admin.firestore().collection('users').doc(user.uid).set({
+//       email: user.providerData[0].email,
+//       displayName: user.providerData[0].displayName,
+//       billing: false
+//     })
+//   }else{
+//     return null;
+//   }
+// });
 
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
