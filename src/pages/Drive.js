4afe88ad4c5be6b770282af7763978884ad6d9cb
perry <pasibeybonsu@drew.edu>
2020-09-05 00:31:20 -0400

    Drive listening to documents state, dispatch attached, layout refined

--- src/pages/Drive.js
+++ src/pages/Drive.js
@@ -1,5 +1,10 @@
-import React, {useState} from "react";
+import React, {useState, useEffect} from "react";
+import {connect} from "react-redux";
+import firebase from "../firebase";
 import classnames from "classnames";
+
+import {setDocumentData} from "../actions";
+import GridBuilder from "../layouts/GridBuilder";
 import Uploader from "../components/Uploader";
 import emptyDrive from "../graphics/emptyDrive.svg";
 
@@ -9,13 +14,45 @@ import {
 } from "reactstrap";
 
 function Drive(props){
+  const {user, setData, documents} = props;
+
   const [toggleView, setToggle] = useState({
-    button: 0
+    grid: 0
   })
   const [modal, setModal] = useState(false);
 
   const handleClose = () => setModal(false);
   const handleOpen = () => setModal(true);
+
+  var unsubscribe = firebase.firestore().collection("users").doc(user.uid)
+                  .collection("drive").onSnapshot(function(snapshot){
+                    var data = [];
+                    snapshot.forEach(function(doc){
+                      data.push({id: doc.id, data: doc.data()});
+                    })
+                    setData(data);
+                  })
+
+  function renderDocs(layout){
+    if(documents){
+      switch (layout.grid) {
+        case 0:
+          return <GridBuilder/>
+        case 1:
+          return <div>List</div>
+        default:
+          return <img className="w-50 " src={emptyDrive} alt="Empty Drive"/>
+      }
+    }else{
+      return <img className="w-50 " src={emptyDrive} alt="Empty Drive"/>
+    }
+  }
+
+  useEffect(() => {
+
+    return () => unsubscribe();
+  // eslint-disable-next-line
+  }, [])
 
   return (
       <>
@@ -24,32 +61,45 @@ function Drive(props){
             <i className="fas fa-plus"></i> Upload Media
           </Button>
           <ButtonGroup className="btn-group-toggle mt-3 mr-4" data-toggle="buttons">
-          <Button className={classnames({ active: toggleView.button === 0 })} color="secondary" onClick={() => setToggle({ button: 0 })}>
+          <Button className={classnames({ active: toggleView.grid === 0 })} color="secondary" onClick={() => setToggle({ grid: 0 })}>
             <input
               autoComplete="off"
               name="gridView"
               type="radio"
-              value={toggleView.button === 0}
+              value={toggleView.grid === 0}
             />
             <span><i className="fas fa-th-large pr-2"></i>Grid</span>
           </Button>
-          <Button className={classnames({ active: toggleView.button === 1 })} color="secondary" onClick={() => setToggle({ button: 1 })}>
+          <Button className={classnames({ active: toggleView.grid === 1 })} color="secondary" onClick={() => setToggle({ grid: 1 })}>
             <input
               autoComplete="off"
               name="listView"
               type="radio"
-              value={toggleView.button === 1}
+              value={toggleView.grid === 1}
             />
             <span><i className="fas fa-list pr-2"></i>List</span>
           </Button>
         </ButtonGroup>
         </div>
-        <div className="text-center">
-          <img className="w-50 " src={emptyDrive} alt="Empty Drive"/>
+        <div className="px-3 pt-4 overflow-auto">
+          {renderDocs(toggleView)}
           <Uploader modal={modal} handleClose={handleClose}/>
         </div>
       </>
     );
   }
 
-export default Drive;
+  function mapStateToProps(state){
+    return{
+      user: state.auth.user,
+      documents: state.drive.documents
+    };
+  }
+
+  const mapDispatchToProps = dispatch => {
+    return{
+      setData: (docs) => dispatch(setDocumentData(docs)),
+    }
+  }
+
+export default connect(mapStateToProps, mapDispatchToProps)(Drive);
