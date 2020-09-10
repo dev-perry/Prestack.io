116921d7b11f447ed410d1dbd0baf358b8ac61ef
perry <pasibeybonsu@drew.edu>
2020-09-09 21:17:36 -0400

    Return date from timestamp object function added

--- src/firebase/actions.js
+++ src/firebase/actions.js
@@ -1,6 +1,13 @@
 import firebase from "./index";
 import {receiveLogin, loginError} from "../actions"
 
+//Function that returns date when passed Firestore timestamp object
+export const getDate = (date) => {
+    const options = {month: 'short', year: 'numeric', day: 'numeric' }
+    var dateStamp = new Date(date.seconds * 1000);
+    return dateStamp.toLocaleDateString(undefined, options);
+  }
+
 //For provider sign-in
 export const providerSignIn = (provider) => dispatch => {
   firebase.auth().signInWithPopup(provider).then(function(result){
