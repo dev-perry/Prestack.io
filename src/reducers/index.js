b955a276a5f4191f45463dcb1f9b875e5dba69ca
perry <pasibeybonsu@drew.edu>
2020-09-09 21:20:51 -0400

    Presentations reducer added to combineReducers

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,9 +1,10 @@
 import {combineReducers} from "redux";
 import auth from "./auth";
 import drive from "./drive";
+import presentations from "./presentations";
 import ReduxStore from "@uppy/store-redux";
 
-const reducers = {auth, drive};
+const reducers = {auth, drive, presentations};
 
 export default combineReducers({
   ...reducers,
