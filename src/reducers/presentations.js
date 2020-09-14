0327d9a2478430bc838e8db0ef76f0bf0975b30d
perry <pasibeybonsu@drew.edu>
2020-09-14 19:58:20 -0400

    CurrentPres state added to reducer

--- src/reducers/presentations.js
+++ src/reducers/presentations.js
@@ -1,14 +1,21 @@
 import{
   CREATE_REQUEST,
   CREATE_SUCCESS,
-  CREATE_FAILURE
+  CREATE_FAILURE,
+  SET_PRES
 } from "../actions";
 
 export default(state = {
   isCreating: false,
-  creationFailure: false
+  creationFailure: false,
+  currentPres: {}
 }, action) => {
   switch(action.type){
+    case SET_PRES:
+      return{
+        ...state,
+        currentPres: action.pres
+      }
     case CREATE_REQUEST:
       return{
         ...state,
