e56fbb353ad62f286ede40dd19eb765bb3e4f900
perry <pasibeybonsu@drew.edu>
2020-09-16 21:50:19 -0400

    Module configurations stored in single object array

---
+++ src/modules.js
@@ -0,0 +1,36 @@
+const modules = [
+  {
+    label: "Drive Asset",
+    icon: "fas fa-hdd",
+    build:{
+        label: "Drive Asset",
+        type: "drive"
+      }
+  },
+  {
+    label: "Webage View",
+    icon: "fas fa-globe",
+    build:{
+      label: "Webpage View",
+      type: "internet"
+    }
+  },
+  {
+    label: "YouTube Video",
+    icon: "fab fa-youtube",
+    build: {
+      label: "YouTube Video",
+      type: "youtube"
+    }
+  },
+  {
+    label: "Participation Module",
+    icon: "fas fa-user-check",
+    build: {
+      label: "Participation Module",
+      type: "participation"
+    }
+  }
+];
+
+export default modules;
