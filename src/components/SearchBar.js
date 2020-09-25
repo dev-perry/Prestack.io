4866b909d1b61159462a2df6e6102d1a2d667782
perry <pasibeybonsu@drew.edu>
2020-09-24 22:04:05 -0400

    Search bar configured with Algolia

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useState} from "react";
 import {
   Form,
   FormGroup,
@@ -7,54 +7,103 @@ import {
   InputGroupAddon,
   InputGroupText
 } from "reactstrap";
-import classnames from "classnames";
-
+import {
+  // connectSearchBox,
+  connectAutoComplete,
+  connectStateResults,
+  // connectHits,
+  InstantSearch,
+  Hits
+  // Index,
+  // Configure,
+} from "react-instantsearch-dom";
+// import Autosuggest from "react-autosuggest";
 import {useLocation} from "react-router-dom";
+import {connect} from "react-redux";
 
-function SearchBar(props){
+import classnames from "classnames";
+import algoliasearch from "algoliasearch/lite";
+
+const ALGOLIA_ID = process.env.REACT_APP_ALGOLIA_ID;
+
+function SearchBar(props) {
+  const {searchKey} = props;
+
+  const [index, setIndex] = useState('drive');
 
   let location = useLocation();
 
   const collectionSearch = () => {
     switch (location.pathname) {
       case "/u/drive":
+        setIndex('drive');
         return "in Drive"
       case "/u/presentations":
+        setIndex('presentations');
         return "Presentations"
       default:
         return null
-
     }
   }
 
-  return(
-    <Form
-      className={classnames(
-        "navbar-search form-inline mr-sm-3",
-        { "navbar-search-light": props.theme === "dark" },
-        { "navbar-search-dark": props.theme === "light" }
-      )}
-    >
-      <FormGroup className="mb-0">
-        <InputGroup className="input-group-alternative input-group-merge">
-          <InputGroupAddon addonType="prepend">
-            <InputGroupText>
-              <i className="fas fa-search" />
-            </InputGroupText>
-          </InputGroupAddon>
-          <Input placeholder={`Search ${collectionSearch()}`} type="text" />
-        </InputGroup>
-      </FormGroup>
-      <button
-        aria-label="Close"
-        className="close"
-        type="button"
-        onClick={props.closeSearch}
-      >
-        <span aria-hidden={true}>×</span>
-      </button>
-    </Form>
+  const Results = connectStateResults(({ searchState, searchResults, children }) =>
+  searchState && searchState.query ? (
+        searchResults && searchResults.nbHits !== 0 ? (
+          children
+        ) : (
+          <div>No matching results.</div>
+        )
+  ) : (
+    null
   )
+);
+
+  const SearchBox = ({currentRefinement, refine, hits}) => {
+
+    return (
+        <Form role="search" className={classnames("navbar-search form-inline mr-sm-3", {
+            "navbar-search-light": props.theme === "dark"
+          }, {
+            "navbar-search-dark": props.theme === "light"
+          })}>
+          <FormGroup className="mb-0">
+            <InputGroup className="input-group-alternative input-group-merge">
+              <InputGroupAddon addonType="prepend">
+                <InputGroupText>
+                  <i className="fas fa-search"/>
+                </InputGroupText>
+              </InputGroupAddon>
+              <Input
+                placeholder={`Search ${collectionSearch()}`}
+                type="search"
+                value={currentRefinement}
+                onChange={e => refine(e.currentTarget.value)}/>
+            </InputGroup>
+          </FormGroup>
+          <Results>
+            <Hits/>
+          </Results>
+          <button aria-label="Close" className="close" type="button" onClick={props.closeSearch}>
+            <span aria-hidden={true}>×</span>
+          </button>
+        </Form>
+  )
+  }
+
+  const SearchBar = connectAutoComplete(SearchBox);
+
+  const client = algoliasearch(ALGOLIA_ID, searchKey);
+
+  return (
+    <InstantSearch searchClient={client} indexName={index}>
+    <SearchBar/>
+  </InstantSearch>
+)
+
 }
 
-export default SearchBar;
+function mapStateToProps(state) {
+  return {searchKey: state.auth.searchKey}
+}
+
+export default connect(mapStateToProps)(SearchBar);
