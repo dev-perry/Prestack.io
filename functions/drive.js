b927b1ce82aeca586172153af4dbd978637e3215
perry <pasibeybonsu@drew.edu>
2020-09-24 21:48:27 -0400

    Search key from Algolia integrated

--- functions/drive.js
+++ functions/drive.js
@@ -9,7 +9,6 @@ const admin = require('firebase-admin');
 //External APIs
 const ALGOLIA_ID = functions.config().algolia.app_id;
 const ALGOLIA_ADMIN_KEY = functions.config().algolia.api_key;
-const ALGOLIA_SEARCH_KEY = functions.config().algolia.search_key;
 
 const ALGOLIA_INDEX_NAME = "drive";
 const client = algoliasearch(ALGOLIA_ID, ALGOLIA_ADMIN_KEY);
