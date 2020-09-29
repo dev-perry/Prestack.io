8d7c926ac0ff22b9e35ee86235e776b7235760ec
perry <pasibeybonsu@drew.edu>
2020-09-28 23:42:41 -0400

    Search reducer added to exports

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -2,9 +2,10 @@ import {combineReducers} from "redux";
 import auth from "./auth";
 import drive from "./drive";
 import presentations from "./presentations";
+import search from "./search";
 import ReduxStore from "@uppy/store-redux";
 
-const reducers = {auth, drive, presentations};
+const reducers = {auth, drive, presentations, search};
 
 export default combineReducers({
   ...reducers,
