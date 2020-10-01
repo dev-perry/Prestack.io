f3cb3763478c36056d84ef0511c44dbbe3b8b89d
perry <pasibeybonsu@drew.edu>
2020-10-01 19:52:33 -0400

    Presentation functions added to function file

--- functions/index.js
+++ functions/index.js
@@ -2,5 +2,6 @@ const admin = require('firebase-admin');
 admin.initializeApp();
 
 exports.drive = require('./drive');
+exports.presentations = require('./presentations');
 exports.users = require('./users');
 exports.search = require('./search');
