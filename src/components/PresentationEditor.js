b6dde9cc764483e045a7a3d49743cb626b82d34c
perry <pasibeybonsu@drew.edu>
2020-10-03 18:01:34 -0400

    Presentation editor buttons configured for building

--- src/components/PresentationEditor.js
+++ src/components/PresentationEditor.js
@@ -5,15 +5,54 @@ import {DragDropContext} from "react-beautiful-dnd";
 
 import ModuleList from "../layouts/ModuleList";
 import modules from "../modules";
-import {setPresentation} from "../actions";
+import {setPresentation, setConstruct} from "../actions";
 import emptySequence from "../graphics/emptySequence.svg";
 
 function PresentationEditor(props) {
-  const {pres, setPres} = props;
+  const {pres, setPres, setBuild} = props;
   const [build, updateBuild] = useState([]);
+  const [modal, updateModal] = useState({
+    open: false,
+    content: null
+  })
 
-  const addtoBuild = (module) => {
-    updateBuild(build.concat(module));
+  function addtoBuild(construct) {
+    updateBuild(build.concat(construct));
+    closeModal();
+  }
+
+  const openModal = (build, form) => {
+    setBuild(build);
+    updateModal({
+      open: true,
+      content: form(addtoBuild)
+  })
+};
+
+  const closeModal = () => {
+    setBuild({});
+    updateModal({
+    open: false,
+    content: null
+  })}
+
+  const BuildDetail = () => {
+    return (
+      <Modal
+        className="modal-dialog"
+        isOpen={modal.open}
+        backdrop={"static"}
+        >
+      <div className="modal-header">
+        <button aria-label="Close" className="close" data-dismiss="modal" type="button" onClick={closeModal}>
+          <span aria-hidden={true}>×</span>
+        </button>
+      </div>
+      <div className="modal-body">
+        {modal.content}
+      </div>
+    </Modal>
+  )
   }
 
   useEffect(() => {
@@ -29,13 +68,10 @@ function PresentationEditor(props) {
   const onDragEnd = result => {
     //TODO: Reorder tiles
     const {destination, source} = result;
-    if(!destination){
+    if (!destination) {
       return;
     }
-    if(
-      destination.droppableId === source.droppableId &&
-      destination.index === source.index
-    ){
+    if (destination.droppableId === source.droppableId && destination.index === source.index) {
       return;
     }
     const resultArray = build;
@@ -47,10 +83,10 @@ function PresentationEditor(props) {
   }
 
   return (
-    <DragDropContext
-      onDragEnd = {onDragEnd}
-      >
-      <Modal className="modal-dialog modal-lg" backdrop="static" isOpen={props.open} toggle={() => props.toggle(!props.open)} unmountOnClose={true}>
+    <>
+    <BuildDetail/>
+    <DragDropContext onDragEnd={onDragEnd}>
+    <Modal className="modal-dialog modal-lg" backdrop="static" isOpen={props.open} toggle={() => props.toggle(!props.open)} unmountOnClose={true}>
       <div className="modal-header">
         <div>
           <h6 className="modal-title" id="modal-title-default">
@@ -75,17 +111,18 @@ function PresentationEditor(props) {
         <div className="py-2 text-center">Add modules:</div>
         <div className="d-flex w-90 justify-content-center">
           {
-            modules.map(function(module, index){
+            modules.map(function(module, index) {
               return (
-                <Button key={index} onClick={()=>{
-                        addtoBuild(module.build)
-                        }}
-                        className="rounded-pill" color="default" outline="outline">
-                        <span>
-                          <i className={`${module.icon} pr-2`}></i>{module.label}
-                        </span>
-                  </Button>
-                );
+                <Button key={index}
+                  onClick={() => {
+                  openModal(module.build, module.form)
+                  }}
+                  className="rounded-pill" color="default" outline="outline">
+                <span>
+                  <i className={`${module.icon} pr-2`}></i>{module.label}
+                </span>
+              </Button>
+            );
             })
           }
         </div>
@@ -93,9 +130,9 @@ function PresentationEditor(props) {
       <div className="modal-footer">
         <Button className="mr-auto" color="link" data-dismiss="modal" type="button"
           onClick={() => {
-          props.toggle(false);
-          updateBuild([]);
-        }}>
+            props.toggle(false);
+            updateBuild([]);
+          }}>
           Close
         </Button>
         <Button color="primary" type="button">
@@ -104,17 +141,21 @@ function PresentationEditor(props) {
 
       </div>
     </Modal>
-    </DragDropContext>
+  </DragDropContext>
+</>
 )
 }
 
 function mapStateToProps(state) {
-  return {pres: state.presentations.currentPres}
+  return {
+    pres: state.presentations.currentPres
+  }
 }
 
 const mapDispatchToProps = dispatch => {
   return {
-    setPres: (pres) => dispatch(setPresentation(pres))
+    setPres: (pres) => dispatch(setPresentation(pres)),
+    setBuild: (build) => dispatch(setConstruct(build))
   }
 }
 
