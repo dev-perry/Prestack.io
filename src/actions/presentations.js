b23293b4dd6cad42d58ea78af6f0b9cba50f5db6
perry <pasibeybonsu@drew.edu>
2020-10-04 22:45:56 -0400

    Sequence update actions and creators configured

--- src/actions/presentations.js
+++ src/actions/presentations.js
@@ -10,6 +10,10 @@ export const SET_BUILD = "SET_BUILD";
 export const CREATE_REQUEST = "CREATE_REQUEST";
 export const CREATE_SUCCESS = "CREATE_SUCCESS";
 export const CREATE_FAILURE = "CREATE_FAILURE";
+//Following for presentation sequence updates
+export const UPDATE_SEQUENCE = "UPDATE_SEQUENCE";
+export const SEQUENCE_FAILURE = "SEQUENCE_FAILURE";
+export const SEQUENCE_SUCCESS = "SEQUENCE_SUCCESS";
 
 //Action for setting presentation
 const setPres = (pres) => {
@@ -45,6 +49,25 @@ const requestFailure = (error) => {
   }
 }
 
+//Actions for update
+const updateSequence = () => {
+  return{
+    type: UPDATE_SEQUENCE
+  };
+}
+const sequenceSuccess = () => {
+  return{
+    type: SEQUENCE_SUCCESS
+  };
+}
+const sequenceFailure = (error) => {
+  console.log(error);
+  return{
+    type: SEQUENCE_FAILURE,
+    error
+  };
+}
+
 //Action creator for setting presentation
 export const setPresentation = pres => dispatch =>{
   //set reducer
@@ -77,5 +100,24 @@ export const createPresentation = (pres) => (dispatch, getState) => {
   .catch(function(error){
     //Indicate an error occurred
     dispatch(requestFailure(error));
+  })
+}
+
+//Action creator for presentation update
+export const updatePresSequence = (sequence, id) => (dispatch, getState) => {
+  // Indicate presentation is being updated
+  dispatch(updateSequence());
+  const state = getState();
+  db.collection("users").doc(state.auth.user.uid).collection("presentations").doc(id)
+  .update({
+    sequence: sequence
+  })
+  .then(function(docRef){
+    //Indicate presentation sequence update was successful
+    dispatch(sequenceSuccess());
+  })
+  .catch(function(error){
+    //Indicate an error occurred
+    dispatch(sequenceFailure())
   })
 }
