ac84c589835a0fd4858eb9a33bab3cecc1ffe19a
perry <pasibeybonsu@drew.edu>
2020-10-08 03:43:46 -0400

    Routes modified to facilitate slideshow route

--- src/App.js
+++ src/App.js
@@ -1,18 +1,30 @@
 import React from "react";
-import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
+import {BrowserRouter as Router, Switch, Route, Redirect} from 'react-router-dom';
+import {connect} from "react-redux";
+import PrivateRoute from "./components/PrivateRoute";
+import SlideShow from "./pages/SlideShow";
 
 import Core from './layouts/Core';
 import Auth from './pages/Auth';
 
-function App() {
+function App(props) {
   return (
     <Router>
       <Switch>
         <Route path="/auth" render={props => <Auth {...props}/>} exact/>
-        <Route path="/u" render={props => <Core {...props}/>}/>
+        <Route path="/c" render={props => <Core {...props}/>}/>
+        <PrivateRoute path="/s/:presid" component={SlideShow} isAuthenticated={props.isAuthenticated} isVerifying={props.isVerifying} exact/>
+        <Redirect from="*" to="/"/>
       </Switch>
     </Router>
   );
 }
 
-export default App;
+function mapStateToProps(state){
+  return{
+    isAuthenticated: state.auth.isAuthenticated,
+    isVerifying: state.auth.isVerifying
+  }
+}
+
+export default connect(mapStateToProps)(App);
