8fd15c1f4dd82c5334585574428cd5520f004d21
perry <pasibeybonsu@drew.edu>
2020-10-10 00:43:25 -0400

    copyScripts and copyStyles removed due to react-new-window integration

--- src/tools/copyScripts.js
+++
@@ -1,6 +0,0 @@
-export default function copyScripts(source, target){
-  Array.from(source.body.querySelectorAll('script[crossorigin="anonymous"]'))
-  .forEach( link => {
-    target.body.appendChild(link.cloneNode(true));
-  })
-}
--- src/tools/copyStyles.js
+++
@@ -1,7 +0,0 @@
-//Code Source: https://medium.com/@rkichenama/should-pull-all-internal-and-external-stylesheets-in-sourcedoc-and-clone-them-into-targetdoc-in-3fced02ae28a
-export default function copyStyles(source, target){
-  Array.from(source.querySelectorAll('link[rel="stylesheet"], style'))
-  .forEach( link => {
-    target.head.appendChild(link.cloneNode(true));
-  })
-}
