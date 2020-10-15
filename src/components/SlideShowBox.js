05879c4f88a9b02834a111ea5b2e726ffa45375a
perry <pasibeybonsu@drew.edu>
2020-10-14 23:15:49 -0400

    SlideShow box reads presentation from database on mount

--- src/components/SlideShowBox.js
+++ src/components/SlideShowBox.js
@@ -1,17 +1,23 @@
-import React, {useState} from "react";
+import React, {useEffect, useState} from "react";
+import {useParams} from "react-router-dom";
 import {
   Carousel,
   CarouselItem,
   CarouselControl,
   CarouselIndicators,
 } from 'reactstrap';
+import {connect} from "react-redux";
+import firebase from "../firebase";
 import SlideBuilder from "../layouts/SlideBuilder";
 
 
 function SlideShowBox(props){
-  const {items} = props;
+  const {user} = props;
+  const db = firebase.firestore();
   const [activeIndex, setActiveIndex] = useState(0);
   const [animating, setAnimating] = useState(false);
+  const [items, setSeq] = useState();
+  const {showid} = useParams();
 
   const next = () => {
     if (animating) return;
@@ -30,31 +36,53 @@ function SlideShowBox(props){
     setActiveIndex(newIndex);
   }
 
-  const slides = items.map((item,index) => {
-    return (
-      <CarouselItem
-        onExiting={() => setAnimating(true)}
-        onExited={() => setAnimating(false)}
-        key={index}
-      >
-        <SlideBuilder module={item}/>
-      </CarouselItem>
-    );
-  });
+  useEffect(()=>{
+    db.collection("users").doc(user.uid).collection("presentations").doc(showid).get().then(function(doc) {
+      setSeq(doc.data().sequence)
+      console.log(doc.data().sequence);
+    }).catch(function(error) {
+      console.log("Error getting document ", error);
+    })
+  //eslint-disable-next-line
+  },[])
 
   return (
-    <Carousel
-      activeIndex={activeIndex}
-      next={next}
-      previous={previous}
-      interval={false}
-    >
-      <CarouselIndicators items={items} activeIndex={activeIndex} onClickHandler={goToIndex} />
-      {slides}
-      <CarouselControl direction="prev" directionText="Previous" onClickHandler={previous} />
-      <CarouselControl direction="next" directionText="Next" onClickHandler={next} />
-    </Carousel>
+    <div className="vh-100 vw-100">
+    {
+      items != null ?
+      <Carousel
+        activeIndex={activeIndex}
+        next={next}
+        previous={previous}
+        interval={false}
+      >
+        <CarouselIndicators items={items} activeIndex={activeIndex} onClickHandler={goToIndex} />
+        {
+          items.map((item,index) => {
+            return (
+              <CarouselItem
+                onExiting={() => setAnimating(true)}
+                onExited={() => setAnimating(false)}
+                key={index}
+              >
+                <SlideBuilder module={item}/>
+              </CarouselItem>
+            );
+          })
+        }
+        <CarouselControl direction="prev" directionText="Previous" onClickHandler={previous} />
+        <CarouselControl direction="next" directionText="Next" onClickHandler={next} />
+      </Carousel>
+      : <div className="my-auto">Loading slideshow</div>
+    }
+  </div>
   );
 }
 
-export default SlideShowBox;
+function mapStateToProps(state){
+  return{
+    user: state.auth.user
+  }
+}
+
+export default connect(mapStateToProps)(SlideShowBox);
