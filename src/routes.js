74704b6d3b0c7c4e1eeab4b6fe0c38fa52a053c3
perry <pasibeybonsu@drew.edu>
2020-12-26 16:40:01 -0500

    Class page removed from app route structure

--- src/routes.js
+++ src/routes.js
@@ -1,7 +1,6 @@
 import Drive from "./pages/Drive";
 import Presentations from "./pages/Presentations";
 import Participation from "./pages/Participation";
-import Class from "./pages/Class";
 // {
 //   path: "/path-name",
 //   name: "Drive",
@@ -9,33 +8,12 @@ import Class from "./pages/Class";
 //   component: Drive,
 //   layout: "layout-tag"
 // },
-
-function buildRoutes(classList){
-  let collapse = [];
-  if(classList != null && classList.length > 0){
-    collapse = classList.map(function(item){
-      return {
-            path: `/classes/${item.id}`,
-            name: item.name,
-            miniName: item.name.charAt(0),
-            component: Class,
-            layout: "admin"
-      }
-    })
-  }
   const routes = [
     {
       path: "/drive",
       name: "Drive",
       icon: "fas fa-hdd",
       component: Drive,
-      layout: "admin"
-    },
-    {
-      path: "/presentations",
-      name: "Presentations",
-      icon: "fas fa-bolt",
-      component: Presentations,
       layout: "admin"
     },
     {
@@ -46,17 +24,15 @@ function buildRoutes(classList){
       layout: "admin"
     },
     {
-      collapse: true,
-      name: "Classes",
-      icon: "fas fa-school",
-      state: "classesCollapse",
-      views: collapse
-    }
+      path: "/presentations",
+      name: "Presentations",
+      icon: "fas fa-bolt",
+      component: Presentations,
+      layout: "admin"
+    },
   ];
 
-  return routes;
-}
-export default buildRoutes;
+export default routes;
 
 // [
 //   {
